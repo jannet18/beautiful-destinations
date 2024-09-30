90a32d7c71ff6446df65911ad055ac4ef2e081f8
jannet18 <janetngei18@gmail.com>
2024-09-30 23:56:38 +0300

    add: deploy workflow

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/beautiful-destinations/",
+  base: "/beautiful-destinations-client/",
   // server: {
   //   host: "127.0.0.1",
   //   port: 3000,
