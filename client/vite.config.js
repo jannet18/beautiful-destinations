ea2d239f5642cf95ccc934cc2439bb1331484b1a
jannet18 <janetngei18@gmail.com>
2024-09-30 23:29:20 +0300

    github pages configurations

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/beautiful-destinations/",
   // server: {
   //   host: "127.0.0.1",
   //   port: 3000,
